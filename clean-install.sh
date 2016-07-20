98b6f9723b528284659b2707fc0ac5e8a9068c48
ximenpo <ximenpo@jiandan.ren>
2016-07-20 18:10:33 +0800

    clean-install.sh relative -> absolute path.

--- clean-install.sh
+++ clean-install.sh
@@ -1,5 +1,7 @@
 # ! /bin/bash
 
-rm  -f  wwwroot/Application/Common/Conf/config.php
-rm  -f  wwwroot/Application/User/Conf/config.php
-rm  -f  wwwroot/Data/install.lock
+CURR_DIR=$(dirname $0)
+
+rm  -f  ${CURR_DIR}/wwwroot/Application/Common/Conf/config.php
+rm  -f  ${CURR_DIR}wwwroot/Application/User/Conf/config.php
+rm  -f  ${CURR_DIR}wwwroot/Data/install.lock
