bbb79b02a8804d5adf024fd93d11d58328c009cd
ximenpo <ximenpo@jiandan.ren>
2016-07-21 14:17:50 +0800

    fix clean-install.sh bugs.

--- clean-install.sh
+++ clean-install.sh
@@ -1,7 +1,7 @@
 # ! /bin/bash
 
-CURR_DIR=$(dirname $0)
+CURR_DIR=$(cd `dirname $0`; pwd)
 
 rm  -f  ${CURR_DIR}/wwwroot/Application/Common/Conf/config.php
-rm  -f  ${CURR_DIR}wwwroot/Application/User/Conf/config.php
-rm  -f  ${CURR_DIR}wwwroot/Data/install.lock
+rm  -f  ${CURR_DIR}/wwwroot/Application/User/Conf/config.php
+rm  -f  ${CURR_DIR}/wwwroot/Data/install.lock
